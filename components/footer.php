5d333c0c836e0f8798935ca8880fed2635bb52c3
revanrevian <131555443+revanrevian@users.noreply.github.com>
2023-10-24 17:05:40 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Kontak Kami</h3>
+         <p>if330_kelompok1@gmail.com</p>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Jam Operasional</h3>
+         <p>08:00AM to 11:00PM </p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Alamat Kami</h3>
+         <a href="https://www.google.com/maps/place/Universitas+Multimedia+Nusantara/@-6.2562185,106.6159875,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69fb56b25975f9:0x50c7d605ba8542f5!8m2!3d-6.2562238!4d106.6185624!16s%2Fm%2F0cc66sf?entry=ttu">
+         Kabupaten Tangerang, Banten</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Hubungi Kami</h3>
+         <p>+62 1111-1111-1111<p>
+      </div>
+
+   </section>
+
+   <div class="credit"><span>Kelompok 1</span> | Hito, Rio, Revan, William</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
